cdac3d73b3103826bfb6650acea4da905e1c3603
Ousmane939 <sosukea200@gmail.com>
2024-10-08 15:43:58 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,19 +2,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "24105114159"
-global.ownername = "𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸"
+global.ownernomer = "22393984430"
+global.ownername = "𝓨𝓸𝓾𝓷𝓰_𝓞𝓾𝓼𝓶𝓪𝓷𝓮🫠"
 global.ytname = "kenzo3146"
 global.socialm = "GitHub: toge012345"
 global.location = "Africa, Gabon, Libreville"
 
-global.ownernumber = '24105114159'  //creator number
-global.ownername = '𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸' //owner name
+global.ownernumber = '22393984430'  //creator number
+global.ownername = '𝓨𝓸𝓲𝓷𝓰_𝓞𝓾𝓼𝓶𝓪𝓷𝓮🫠' //owner name
 global.botname = '𝚃𝙾𝙶𝙴-𝙱𝚄𝙶-𝚅𝟸' //name of the bot
 
 //sticker details
-global.packname = '𝚃𝙾𝙶𝙴-𝙱𝚄𝙶-𝚅𝟸'
-global.author = '𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸'
+global.packname = '🕷️'
+global.author = '🕸️'
 
 //console view/theme
 global.themeemoji = '👾'
